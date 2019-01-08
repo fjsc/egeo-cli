53319df4dc8b3d757fbb04916916e673d5c9e13d
fjsc <fjsevilla@stratio.com>
2019-01-09 00:22:11 +0100

    Demo config

--- template/rollup.config.js
+++ template/rollup.config.js
@@ -1,6 +1,9 @@
 import typescript from 'rollup-plugin-typescript2';
 import livereload from 'rollup-plugin-livereload';
 import sourcemaps from 'rollup-plugin-sourcemaps';
+import cleanup from 'rollup-plugin-cleanup';
+import copy from 'rollup-plugin-copy-glob';
+import resolve from 'rollup-plugin-node-resolve';
 
 import serve from 'rollup-plugin-serve'
 import pkg from './package.json'
@@ -23,12 +26,19 @@ export default {
     ...Object.keys(pkg.peerDependencies || {}),
   ],
   plugins: [
+    cleanup(),
     typescript({
       typescript: require('typescript'),
     }),
+    production && copy([
+      { 
+        files: 'src/demo/*.*', dest: 'dist/demo' 
+    }]),
     sourcemaps(),
-    serve(['src/demo', 'node_modules']),
-    livereload(),
-
-  ],
+    !production && serve({
+      open: true,
+      contentBase: ['dist/']
+    }),
+    !production && livereload(),
+  ]
 }
